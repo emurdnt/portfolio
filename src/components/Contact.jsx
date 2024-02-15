fed24286530ab5339db6f95fc7b2825a54f6bcd4
Emily Urdaneta <emilyurdaneta@Emilys-MacBook-Pro.local>
2024-02-14 20:24:05 -0700

    creating the contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
+import { motion } from 'framer-motion';
+import emailjs from '@emailjs/browser';
+
+import { styles } from '../styles';
+import { SectionWrapper } from '../hoc';
+import { EarthCanvas } from './canvas';
+import { slideIn } from '../utils/motion';
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [loading, setLoading] = useState(false);
 
-export default Contact
+  const handleChange = () => {};
+
+  const handleSubmit = () => {};
+
+  return (
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden"></div>
+  );
+};
+
+export default SectionWrapper(Contact, 'contact');
