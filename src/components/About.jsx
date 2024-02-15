61ba00ae3e420b19c05f7f2d1b738d0dc0f1a5a3
Emily Urdaneta <emilyurdaneta@Emilys-MacBook-Pro.local>
2024-02-14 19:18:22 -0700

    changes in the about section regardging styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,9 +9,10 @@ import { SectionWrapper } from '../hoc';
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
+    //need to tweak this for when it goes into mobile
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
-        variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+        variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
         <div
@@ -24,9 +25,10 @@ const ServiceCard = ({ index, title, icon }) => {
         >
           <img
             src={icon}
-            alt={title}
+            alt="web-development"
             className="w-16 h-16 object-contain"
           />
+
           <h3 className="text-white text-[20px] font-bold text-center">
             {title}
           </h3>
@@ -56,8 +58,12 @@ export const About = () => {
         and products.
       </motion.p>
       <div className="mt-20 flex flex-wrap gap-10">
-        {services.map((service, idx) => (
-          <ServiceCard key={service.title} index={idx} {...service} />
+        {services.map((service, index) => (
+          <ServiceCard
+            key={service.title}
+            index={index}
+            {...service}
+          />
         ))}
       </div>
     </>
